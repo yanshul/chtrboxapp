3d4a3040686bb63a3417f845ab8e0a89fb0f7a27
Sagar <sagar@chtrbox.com>
2019-05-28 17:48:52 +0530

    my first commint

---
+++ platforms/android/app/build/intermediates/assets/debug/www/precache-manifest.ed246da970d9b406219ab765e10e43d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9f5af231c0c1a5e5b0c10e42214585",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ba70d0531e46395021d",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "./static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6ba70d0531e46395021d",
+    "url": "./static/js/main.8c819f96.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ platforms/browser/www/precache-manifest.c71738145c4e4cfdab1b83f349cfa6d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438da289ef1334ff5a5f19b9901645fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee9fd900e2b082a8415",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "aee9fd900e2b082a8415",
+    "url": "/static/js/main.7c025cb2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
